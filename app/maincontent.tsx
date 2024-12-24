f32a067e53a67095c1aa8afeedd5084a28f4f86d
Alqomory01 <Alqomorytech@gmail.com>
2024-12-24 13:14:37 +0100

    color text changed

--- app/maincontent.tsx
+++ app/maincontent.tsx
@@ -27,7 +27,7 @@ export default function Maincontent() {
           <div className='flex items-center justify-start'>
           <input type='text' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} placeholder='search by name' className=' sm:w-[200px] border border-blue-800 rounded-md md:w-[350px] h-8 pl-2' /> <Image src='/copilot.png' alt='copilot.png' width={30} height={30} className='transform -translate-x-8'/></div>
 
-        <div className='grid grid-cols-4 gap-x-4 pb-2 mt-2'>
+        <div className='grid grid-cols-4 gap-x-4 pb-2 mt-2 text-black'>
           <div className='flex items-center'><p>Name</p><RiArrowDropDownLine /></div>
           <div className='flex items-center'><p>Topic</p><RiArrowDropDownLine /></div>
           <div className='flex items-center'><p>Status reason</p><RiArrowDropDownLine /></div>
@@ -44,12 +44,12 @@ export default function Maincontent() {
               <p className="text-blue-800 pl-2">{lead.name}</p>
             </div>
             <div className="flex items-center">
-              <p className="text-sm">{lead.topic}</p>
+              <p className="text-sm text-black">{lead.topic}</p>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center text-black">
               <p>{lead.status}</p>
             </div>
-            <div className="flex items-center">
+            <div className="flex items-center text-black">
               <p>{lead.date}</p>
               <p className="pl-2">{lead.time}</p>
             </div>
@@ -60,72 +60,72 @@ export default function Maincontent() {
     
         <div className='grid grid-cols-4 sm:gap-y-2 gap-x-4  pt-2 pb-2'>
           <div className='flex items-center'><input type='checkbox'/><p className='pl-1 sm:pl-1 text-blue-800  md:pl-2'>Wilford Asher</p></div>
-          <div className='flex items-center'><p className='text-xs pl-1'>Cafe A 100 for commercial use</p></div>
-          <div className='flex items-center'><p className='pl-1'>New</p></div>
-          <div className='flex items-center'><p>4/02/2024</p><p>12:00 PM</p></div>
+          <div className='flex items-center'><p className='text-xs pl-1 text-black'>Cafe A 100 for commercial use</p></div>
+          <div className='flex items-center'><p className='pl-1 text-black'>New</p></div>
+          <div className='flex items-center text-black'><p>4/02/2024</p><p>12:00 PM</p></div>
           
           
         </div>
         <hr/>
         <div className='grid grid-cols-4 gap-x-4 space-x-2 gap-y-3 pt-2 pb-2'>
           <div className='flex items-center'><input type='checkbox'/><p className='pl-1 sm:pl-1 text-blue-800 md:pl-2'>Joshia Love</p></div>
-          <div className='flex items-center'><p className='text-xs pl-1'>Upgrading service plan</p></div>
-          <div className='flex items-center'><p className='pl-1'>New</p></div>
-          <div className='flex items-center'><p>3/30/2024</p><p>07:45 AM</p></div>
+          <div className='flex items-center'><p className='text-xs pl-1 text-black'>Upgrading service plan</p></div>
+          <div className='flex items-center'><p className='pl-1 text-black'>New</p></div>
+          <div className='flex items-center text-black'><p>3/30/2024</p><p>07:45 AM</p></div>
           
           
         </div>
         <hr/>
         <div className='grid grid-cols-4 gap-x-4 pt-2 pb-2'>
           <div className='flex items-center'><input type='checkbox'/><p className='pl-1 sm:pl-1 text-blue-800 md:pl-2'>Haris Curtis</p></div>
-          <div className='flex items-center'><p className='text-xs pl-1'>Issue with througout on Epiresso master</p></div>
-          <div className='flex items-center'><p className='pl-1'>New</p></div>
-          <div className='flex items-center'><p>3/28/2024</p><p>3:30 PM</p></div>
+          <div className='flex items-center'><p className='text-xs pl-1 text-black'>Issue with througout on Epiresso master</p></div>
+          <div className='flex items-center'><p className='pl-1 text-black'>New</p></div>
+          <div className='flex items-center text-black'><p>3/28/2024</p><p>3:30 PM</p></div>
           
           
         </div>
         <hr/>
         <div className='grid grid-cols-4 gap-x-4 pt-2 pb-2'>
           <div className='flex items-center'><input type='checkbox'/><p className='pl-1 sm:pl-1 text-blue-800 md:pl-2'>Jerman Bernet</p></div>
-          <div className='flex items-center'><p className='text-xs pl-1'>New Roster distribution facility</p></div>
-          <div className='flex items-center'><p className='pl-1'>New</p></div>
-          <div className='flex items-center'><p>3/25/2024</p><p>11:05 AM</p></div>
+          <div className='flex items-center'><p className='text-xs pl-1 text-black'>New Roster distribution facility</p></div>
+          <div className='flex items-center'><p className='pl-1 text-black'>New</p></div>
+          <div className='flex items-center text-black'><p>3/25/2024</p><p>11:05 AM</p></div>
           
           
         </div>
         <hr/>
         <div className='grid grid-cols-4 gap-x-4 pt-2 pb-2'>
           <div className='flex items-center'><input type='checkbox'/><p className='pl-1 text-xs sm:pl-1 text-blue-800 md:pl-2'>Gerald step</p></div>
-          <div className='flex items-center'><p className='text-xs pl-1'>Concerns current Machines</p></div>
-          <div className='flex items-center'><p className='pl-1'>New</p></div>
-          <div className='flex items-center'><p>3/23/2024</p><p>4:50 PM</p></div>
+          <div className='flex items-center'><p className='text-xs pl-1 text-black'>Concerns current Machines</p></div>
+          <div className='flex items-center'><p className='pl-1 text-black'>New</p></div>
+          <div className='flex items-center text-black'><p>3/23/2024</p><p>4:50 PM</p></div>
           
           
         </div>
         <hr/>
         <div className='grid grid-cols-4 gap-x-4 pt-2 pb-2'>
           <div className='flex items-center'><input type='checkbox'/><p className='pl-1 sm:pl-1 text-blue-800 md:pl-2'>Hale Griffits</p></div>
-          <div className='flex items-center'><p className='text-xs pl-1'>Expanding Business</p></div>
-          <div className='flex items-center'><p className='pl-1'>New</p></div>
-          <div className='flex items-center'><p>3/23/2024</p><p>10:20 AM</p></div>
+          <div className='flex items-center'><p className='text-xs pl-1 text-black'>Expanding Business</p></div>
+          <div className='flex items-center'><p className='pl-1 text-black'>New</p></div>
+          <div className='flex items-center text-black'><p>3/23/2024</p><p>10:20 AM</p></div>
           
           
         </div>
         <hr/>
         <div className='grid grid-cols-4 gap-x-4 pt-2 pb-2'>
           <div className='flex items-center'><input type='checkbox'/><p className='pl-1 sm:pl-1 text-blue-800 md:pl-2'>Rachel Micheal</p></div>
-          <div className='flex items-center'><p className='text-xs pl-1'>Addressing service concerns</p></div>
-          <div className='flex items-center'><p className='pl-1'>New</p></div>
-          <div className='flex items-center'><p>3/19/2024</p><p>1:15 PM</p></div>
+          <div className='flex items-center'><p className='text-xs pl-1 text-black'>Addressing service concerns</p></div>
+          <div className='flex items-center'><p className='pl-1 text-black'>New</p></div>
+          <div className='flex items-center text-black'><p>3/19/2024</p><p>1:15 PM</p></div>
           
           
         </div>
         <hr/>
         <div className='grid grid-cols-4 gap-x-4 pt-2 pb-2'>
           <div className='flex items-center'><input type='checkbox'/><p className='pl-1 sm:pl-1 text-blue-800 md:pl-2'>Alex Baker</p></div>
-          <div className='flex items-center'><p className='text-xs pl-1'>Premium Coffee Beans</p></div>
-          <div className='flex items-center'><p className='pl-1'>New</p></div>
-          <div className='flex items-center'><p>3/17/2024</p><p>8:00 AM</p></div>
+          <div className='flex items-center'><p className='text-xs pl-1 text-black'>Premium Coffee Beans</p></div>
+          <div className='flex items-center'><p className='pl-1 text-black'>New</p></div>
+          <div className='flex items-center text-black'><p>3/17/2024</p><p>8:00 AM</p></div>
           
           
         </div>
